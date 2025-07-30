03a09b45d762bd958fd038de2ed708d361ec2595
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-07-30 17:28:34 -0300

    Criado um array em js

---
+++ js/03-meu-script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// Declarando um arrayn(vetor)
+
+let vocalistas = ["Ozzy", "Dio", "Lemmy", "David Gilmour"]
+
+// Busca de um elemento no vetor por meio de indice
+//console.log(vocalistas[0])
+
+// Retorna todos os elementos do vetor, mas retorna apenas no console/ apenas um quebragalho
+//console.log(vocalistas)
+
+// o David Gilmour está em turnê tocando Pink Flouyd
+console.log(`O ${vocalistas[3]} está em turnê tocando Pik Floyd`)
