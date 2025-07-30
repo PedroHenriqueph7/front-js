b6fb7da960c2739f301baf58953cdce3c6ec0be8
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-07-30 16:09:25 -0300

    Realizado exercicio de concatenação de texto com variaveis

---
+++ js/02-meu-script.js
@@ -0,0 +1,8 @@
+/* Exercicios*/
+
+// 1) Crie uma variavel chamada "uc" e coloque nela o número da UC que estamos
+    let uc = 2
+// 2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
+    let competencia = "conteúdos de Front end, a parte visual de uma página com linguagens HTML, CSS e Java Script"
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
+    console.log(`A Unidade Curricular ${uc} trata-se de ${competencia}.`) 
