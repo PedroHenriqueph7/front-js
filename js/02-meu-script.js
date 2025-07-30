b57aa4b9f52b2d798f73c848c92cdfda72ff30df
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-07-30 16:40:55 -0300

    Marcacao de texto com "" no console l

--- js/02-meu-script.js
+++ js/02-meu-script.js
@@ -3,6 +3,20 @@
 // 1) Crie uma variavel chamada "uc" e coloque nela o número da UC que estamos
     let uc = 2
 // 2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
-    let competencia = "conteúdos de Front end, a parte visual de uma página com linguagens HTML, CSS e Java Script"
+    let competencia = "Front end"
 // 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
-    console.log(`A Unidade Curricular ${uc} trata-se de ${competencia}.`) 
+    console.log(`A Unidade Curricular ${uc} trata-se de ${competencia}.`)
+    
+// Marcação de Texto/ Aspas em um texto
+
+// Errado:
+// console.log("Gosto "muito" de paçoca")
+
+// Correto
+console.log('Gosto "muito" de paçoca')
+
+// Correto
+console.log("Gosto 'muito' de paçoca")
+
+//Correto
+console.log("Gosto \"muito\" de paçoca")
