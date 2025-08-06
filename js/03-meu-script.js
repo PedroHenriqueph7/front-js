aa7da536b87f871ff50348c2a472082c46039c95
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-08-06 15:18:42 -0300

    Inserido  exercicio sobre acesso aos indices de um array

--- js/03-meu-script.js
+++ js/03-meu-script.js
@@ -12,3 +12,19 @@ let vocalistas = ["Ozzy", "Dio", "Lemmy", "David Gilmour"]
 
 // o David Gilmour está em turnê tocando Pink Flouyd
 console.log(`O ${vocalistas[3]} está em turnê tocando Pik Floyd`)
+
+
+// Constantes também podem se tornar arreys
+const coisas = [10, "Senac", 23.4]
+
+/* Exercícios (FAÇA AQUI MESMO!) */
+
+// 1) Crie um array contendo o nome de 7 coisas que vc gosta (exemplos: artistas, músicas, livros, comida... sei lá, inventa!)
+
+let hobbies = ["Futebol", "Formula1", "Academia", "Leitura", "Animes", "Cinema", "Parque"]
+// 2) Em seguida, mostre no console uma frase personalizada indicando o segundo, o quinto e o sétimo elemento do array. Use concatenação e/ou template string.
+
+console.log("Hoje treinarei na "+ hobbies[1] +" a noite e logo após irei assistir "+ hobbies[4] +" e marcarei essa semana uma caminhada ao "+ hobbies[6] +".")
+
+console.log(`Hoje iniciarei um treino na nova ${hobbies[1]} do bairro e assistirei ${hobbies[4]} e irei ao ${hobbies[6]} esta semana`)
+
