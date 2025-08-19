70d2485622744449976b3b10c5e301f8a9250355
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-08-19 15:04:25 -0300

    Inserido Atividade Condicional com if else

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -40,3 +40,28 @@ switch (diaDaSemana) {
        console.log('Dia da semana não encontrado')
         
 }
+
+// Exercicio 
+
+// 1. Crie duas variáveis conforme a seguir:
+ 
+// - Nota 1 (contendo um valor de 0 a 10)
+// - Nota 2 (contendo outro valor de 0 a 10)
+
+let nota1 = 5
+let nota2 = 8
+ 
+// 2. Crie uma variável chamada "Média" que receberá o valor CALCULADO da média das duas notas informadas. 
+// DICA: você deve SOMAR as duas notas e DEPOIS dividir por 2.
+
+let media = (nota1 + nota2) / 2
+
+// 3. Programe uma condicional que verifique o valor da média calculada. Se a média for maior/igual a 7, mostre "aprovado". 
+// Caso contrário, mostre "reprovado".
+
+if (media >= 7)
+    console.log('Aprovado')
+else 
+    console.log('Reprovado')
+
+
