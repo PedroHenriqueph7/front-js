9ed700166d12f21f0d1e490e51300171f5c6b26c
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-08-19 14:36:31 -0300

    Condicionais em JS

---
+++ js/05-condicionais.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let numero = 5
+
+if (numero >= 10) {
+    console.log("Condição Verdadeira")
+} else {
+    console.log("Condição Falsa")
+}
+
+let resultado = (numero >= 10)? true : false
+
+console.log(resultado)
+
