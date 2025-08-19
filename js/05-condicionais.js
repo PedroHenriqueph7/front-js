93cf8f4f9509624761cf1c996cec3a223ae71ae5
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-08-19 14:59:42 -0300

    Condicional Switch Case

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -2,13 +2,41 @@
 
 let numero = 5
 
-if (numero >= 10) {
+if (numero >= 10) 
     console.log("Condição Verdadeira")
-} else {
+else 
     console.log("Condição Falsa")
-}
+
 
 let resultado = (numero >= 10)? true : false
 
+
 console.log(resultado)
 
+let diaDaSemana = 6
+
+switch (diaDaSemana) {
+    case 1:
+        console.log("segunda")
+        break
+    case 2:
+        console.log("terca")
+        break
+    case 3:
+        console.log("quarta")
+        break
+    case 4:
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+        console.log('sabado')
+        break
+    case 7:
+        console.log('domingo')
+    default :
+       console.log('Dia da semana não encontrado')
+        
+}
