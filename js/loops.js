76c4a049dd6f227e476671ae6869316e0edd3ba5
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-08-20 17:27:57 -0300

    Atividade de Loops

--- js/loops.js
+++ js/loops.js
@@ -42,6 +42,8 @@ for (let i = 0; i < bandas.length; i++) {
     
 }
 
+// forEach
+
 const pessoas = [
     {
         nome: "Pessoa1",
@@ -60,9 +62,88 @@ const pessoas = [
         idade: 29
     }
 ]
+
 console.log("\nUtilizando o foreach e uma array de objetos")
+
 pessoas.forEach(element => {
     console.log(element)
 });
 
+//// forOf
+
+let cidades = [
+    {
+        nome: "Sao Paulo",
+        regiaoBrasil: "Sudeste"
+    },
+     {
+        nome: "Rio de Janeiro",
+        regiaoBrasil: "Sudeste"
+    },
+     {
+        nome: "Rio Grande Do Sul",
+        regiaoBrasil: "Sul"
+    },
+     {
+        nome: "Mato Grosso",
+        regiaoBrasil: "Centro Oeste"
+    },
+]
+
+
+console.log("\nUtilizando For of")
+
+for (const cidade of cidades) {
+    console.log(cidade)
+}
+
+console.log("\nExemplo loop e objeto")
+
+const pessoa = {
+    nome: "Fulano da Silva",
+    idade: 40,
+    cidade: "São Paulo",
+    estado: "SP"
+}
+
+for (const prop in pessoa) {
+    console.log(pessoa[prop])
+}
+
+// 1. Faça um array chamado "clientes" contendo 3 objetos.
+// Cada objeto deverá ter uma propriedade "identificador" (com valores 1, 2 e 3) e uma propriedade "nome" (com os nomes dos clientes: "Dio", "Ozzy" e "Ian").
+
+const clientes = [
+    {
+        id: 1,
+        nome: 'Dio'
+    },
+    {
+        id: 2,
+        nome: 'Ozzy'
+    },
+    {
+        id: 3,
+        nome: 'Ian'
+    }
+]
+
+
+// 2. Faça um loop (qualquer um dos que vimos) e mostre no console os dados de cada cliente conforme a seguir:
+ 
+// - Cliente: Dio, id: 1
+// - Cliente: Ozzy, id: 2
+// - Cliente: Ian, id: 3
+
+console.log('\nExemplo no ForOf')
+for (const iterador of clientes) {
+    console.log("- Cliente: "+ iterador.nome +", id: "+ iterador.id)
+}
+
+console.log('\nExemplo no ForIn')
+for (const iterador in clientes) {
+    const cliente = clientes[iterador]
+
+    console.log("- Cliente: "+ cliente.nome +", id: "+ cliente.id)
+}
 
