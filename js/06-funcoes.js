f71932af2fc46fc6e3f81049ba677c557df8a023
Pedro Henrique <169095729+PedroHenriqueph7@users.noreply.github.com>
2025-08-26 17:27:44 -0300

    inserido funcoes e manipulacao de Dom

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -36,3 +36,54 @@ console.log("\nExemplo 3: Arrow Function (Função flecha/Seta)");
 
 /*Sintaxe potencialmente mais simples para funções no JavaScript */
 const exemplo3 = () => {}
+
+console.log("\nExemplo 4: função com parâmetro")
+function saudacao(nome = "Visitante"){
+    console.log("Olá, "+nome)
+}
+
+let nome = "Pedro"
+saudacao(nome)
+
+console.log("\nExemplo 5: função com parâmetros e retorno")
+
+
+/*Sempre que tivermos a necessidade de trabalhar com o resultado do processamento de uma função, então essa função deve ter o return */
+function multiplicar(valor1, valor2) {
+    return valor1 * valor2
+}
+
+let resultado1 = multiplicar (10, 5)
+let resultado2 = multiplicar(200, 10)
+
+console.log("Resultado 1: "+ resultado1)
+console.log("Resultado 2: "+ resultado2)
+
+console.log("\nExemplo 6: Simplificando com Arrow Function")
+
+//Sintaxe declarada/nomeada
+// function somar(valor1, valor2) {
+//     return valor1 + valor2
+// }
+
+// Versão 2: Sintaxe Arrow Function
+const somar = (valor1, valor2) => valor1 + valor2
+
+console.log( somar(150, 500))
+
+console.log("\nExemplo 7: formatando valor monetário")
+
+function formatarMoeda(valor){
+   return new Intl.NumberFormat("pt-br", { style: "currency", currency: "BRL"}).format(valor)
+}
+
+let preco = 500;
+let desconto = preco * 0.10
+let precoFinal = preco - desconto
+
+console.log("Preço original = "+formatarMoeda(precoFinal))
+console.log("Desconto = "+formatarMoeda(desconto))
+console.log("Preço final = "+formatarMoeda(precoFinal))
+
+// Exemplo: usando recursos da classe Intl (Internacionalização)
+ 
